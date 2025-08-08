b4287ac23aeaedf0c5fb40f5d1f7f5f00cce0f23
ashuabhishek1 <43219020+ashuabhishek1@users.noreply.github.com>
2025-08-08 19:25:26 +0530

    removed phone and locale field form customer schema

--- config/schema.sql
+++ config/schema.sql
@@ -1,7 +1,5 @@
 CREATE TABLE customers (
     customer_id VARCHAR(36) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    address VARCHAR(255),
-    phone VARCHAR(255),
-    locale VARCHAR(255)
+    address VARCHAR(255)
 );
