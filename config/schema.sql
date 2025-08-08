221099b8017af1f0f48934a4eada7531a26e8a4e
ashuabhishek1 <43219020+ashuabhishek1@users.noreply.github.com>
2025-08-08 19:21:06 +0530

    update locale field in customer

--- config/schema.sql
+++ config/schema.sql
@@ -2,5 +2,6 @@ CREATE TABLE customers (
     customer_id VARCHAR(36) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     address VARCHAR(255),
-    phone VARCHAR(255)
+    phone VARCHAR(255),
+    locale VARCHAR(255)
 );
